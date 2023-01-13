0618b62bfca2305dd4a287b11885d1469d7f7c0c
tientai44 <97567774+tientai44@users.noreply.github.com>
2023-01-13 13:08:01 +0700

    Update

--- HB-1/Assets/Scripts/PlayerController.cs
+++ HB-1/Assets/Scripts/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         isGrounded = CheckGrounded();
         horizontal = Input.GetAxisRaw("Horizontal");
